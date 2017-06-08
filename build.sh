ba9dfe2414a92e9eb87afb6b28fa8e7b72661cd0
ashic <ashic@live.com>
2017-06-08 12:33:11 +0100

    added node container

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -xe
+
+export REPO=${REPO:-heartysoft/docker-builder-aws}
+export TAG_TO_USE=${TRAVIS_TAG:-snapshot}
+docker build -t $REPO:latest .
+docker tag $REPO:latest $REPO:$TAG_TO_USE
+docker build -t $REPO:latest-node -f DockerfileNode .
+docker tag $REPO:latest-node $REPO:$TAG_TO_USE-node
+
